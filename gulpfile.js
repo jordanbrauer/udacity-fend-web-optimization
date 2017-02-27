95063943d4da66580257988c3a55ffe262d70f93
Jordan Brauer <esales@midcanadafasteners.com>
2017-02-27 12:51:34 -0600

    Feat: CSS build task complete.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 /** Require project information. */
 const pkg = require('./package.json');
+const pkgStyles = Object.keys(pkg.paths.dist.css);
 
 /** Require Gulp and plugins. */
 const gulp = require('gulp');
@@ -11,18 +12,34 @@ const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const del = require('del');
 
+/** CSS Cleaner */
+gulp.task('clean:css', () => {
+  return del(pkg.globs.dist.css);
+});
+
 /** CSS Concat */
 gulp.task('concat:css', () => {
-  /** Get all distributable css destinations. */
-  const styles = Object.keys(pkg.paths.dist.css);
-
   /** Concatenate css files from package globs for each dist css destination. */
-  return styles.forEach((key) => {
+  return pkgStyles.forEach(key => {
     gulp.src(pkg.globs.src.css[key])
       .pipe(concat('styles.css'))
       .pipe(gulp.dest(pkg.paths.dist.css[key]));
   });
 });
+
+/** CSS Minify */
+gulp.task('minify:css', () => {
+  return pkgStyles.forEach(key => {
+    // console.log(pkg.paths.dist.css[key] + 'styles.css');
+    gulp.src(pkg.paths.dist.css[key] + 'styles.css')
+      .pipe(minifyCss())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(pkg.paths.dist.css[key]));
+  });
+});
+
+/** CSS Build */
+gulp.task('build:css', ['clean:css', 'concat:css', 'minify:css']);
 
 /** Default task for testing. */
 gulp.task('default', () => {
