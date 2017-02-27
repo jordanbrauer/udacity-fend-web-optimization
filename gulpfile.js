f24691147233c2929cc4781706ae58aa1514413a
Jordan Brauer <esales@midcanadafasteners.com>
2017-02-27 13:28:47 -0600

    Fix: Reformatting & comments

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,34 @@ const rename = require('gulp-rename');
 const merge = require('merge-stream');
 const del = require('del');
 
-/**
- * CSS Cleaner
- * Clean generated CSS files.
- */
+//===============================================
+// Default
+//===============================================
+
+/** Default - task for testing Gulp installation. */
+gulp.task('default', () => {
+  console.log('Gulp is working!');
+});
+
+//===============================================
+// Cleaners
+//===============================================
+
+/** CSS Cleaner - Delete generated CSS files. */
 gulp.task('clean:css', () => {
   return del(pkg.globs.dist.css);
 });
 
-/**
- * CSS Concat
- * Concatenate css files from package globs for each dist css destination.
- */
+/** HTML Cleaner - Delete generated HTML files. */
+gulp.task('clean:views', () => {
+  return del('./dist/**/*.html');
+});
+
+//===============================================
+// CSS Tasks
+//===============================================
+
+/** CSS Concat - Concatenate css files from package globs for each dist css destination. */
 gulp.task('concat:css', () => {
   return pkgStyles.forEach(key => {
     gulp.src(pkg.globs.src.css[key])
@@ -33,10 +49,7 @@ gulp.task('concat:css', () => {
   });
 });
 
-/**
- * CSS Minify
- * Minify merged css files from package globs for each dist css destination.
- */
+/** CSS Minify - Minify merged css files from package globs for each dist css destination. */
 gulp.task('minify:css', () => {
   return pkgStyles.forEach(key => {
     gulp.src(pkg.paths.dist.css[key] + 'styles.css')
@@ -46,16 +59,10 @@ gulp.task('minify:css', () => {
   });
 });
 
-/**
- * CSS Build
- * Call individual CSS tasks from a single build task.
- */
+/** CSS Build - Call individual CSS tasks from a single build task. */
 gulp.task('build:css', ['clean:css', 'concat:css', 'minify:css']);
 
-/**
- * CSS Injection
- * Inject styles into HTML from CSS files to improve performance.
- */
+/** CSS Injection - Inject styles into HTML from CSS files to improve performance. */
 gulp.task('inject:css', () => {
   /** Copy over most recent HTML files. */
   const stream_a = gulp.src('./src/**/*.html')
@@ -68,9 +75,4 @@ gulp.task('inject:css', () => {
 
   /** Merge and return stream. */
   return merge(stream_a, stream_b);
-});
-
-/** Default task for testing. */
-gulp.task('default', () => {
-  console.log('Gulp is working!');
 });
