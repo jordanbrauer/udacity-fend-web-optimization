c726ece28acebcfdd9634bd71a1ea3762dcadb12
Jordan Brauer <esales@midcanadafasteners.com>
2017-02-27 15:43:12 -0600

    Feat: Build tasks finished.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
-/** Require project information. */
+/** Require project information along with gulp & plugins. */
 const pkg = require('./package.json');
 const pkgStyles = Object.keys(pkg.paths.dist.css);
-
-/** Require Gulp and plugins. */
 const gulp = require('gulp');
 const minifyCss = require('gulp-cssnano');
 const minifyJs = require('gulp-uglify');
-const minifyImg = require('gulp-image');
+const compressImg = require('gulp-image');
 const inlineCss = require('gulp-inline-css');
+const inline = require('gulp-inline');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const merge = require('merge-stream');
 const del = require('del');
 
 //===============================================
-// Default
+// Default Task
 //===============================================
 
 /** Default - task for testing Gulp installation. */
@@ -23,21 +22,36 @@ gulp.task('default', () => {
 });
 
 //===============================================
-// Cleaners
+// Directory Cleaning Tasks
 //===============================================
-
-/** CSS Cleaner - Delete generated CSS files. */
-gulp.task('clean:css', () => {
-  return del(pkg.globs.dist.css);
-});
 
 /** HTML Cleaner - Delete generated HTML files. */
-gulp.task('clean:views', () => {
-  return del('./dist/**/*.html');
+gulp.task('clean:views', () => del(pkg.globs.dist.views));
+
+/** CSS Cleaner - Delete generated CSS files. */
+gulp.task('clean:css', () => del(pkg.globs.dist.css));
+
+/** JS Cleaner - Delete generated JS files. */
+gulp.task('clean:js', () => del(pkg.globs.dist.js));
+
+/** Image Cleaner - Delete generated image files. */
+gulp.task('clean:img', () => del(pkg.globs.dist.img));
+
+/** Super Cleaner - Deletes all generated files. */
+gulp.task('clean:all', ['clean:views', 'clean:css', 'clean:js', 'clean:img'], () => del(pkg.paths.dist.views));
+
+//===============================================
+// HTML View Tasks
+//===============================================
+
+/** Copy Views - Move HTML view files from src to dist - ready for DI. */
+gulp.task('copy:views', () => {
+  gulp.src(pkg.globs.src.views)
+  .pipe(gulp.dest(pkg.paths.dist.views));
 });
 
 //===============================================
-// CSS Tasks
+// CSS Style Tasks
 //===============================================
 
 /** CSS Concat - Concatenate css files from package globs for each dist css destination. */
@@ -59,20 +73,70 @@ gulp.task('minify:css', () => {
   });
 });
 
-/** CSS Build - Call individual CSS tasks from a single build task. */
+//===============================================
+// JS Script Tasks
+//===============================================
+
+/** JS Copy - Move JS files for minification and DI. */
+gulp.task('copy:js', () => {
+  return gulp.src(pkg.globs.src.js)
+    .pipe(gulp.dest(pkg.paths.dist.js));
+});
+
+/** JS Minify - Minify JS files for external use. */
+gulp.task('minify:js', () => {
+  return gulp.src(pkg.globs.dist.js)
+    .pipe(minifyJs())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(pkg.paths.dist.js));
+});
+
+//===============================================
+// Image Tasks
+//===============================================
+
+/** Image Compressions - Compress images from src assets. */
+gulp.task('compress:img', () => {
+  return gulp.src(pkg.globs.src.img)
+    .pipe(compressImg())
+    .pipe(gulp.dest(pkg.paths.dist.img));
+});
+
+//===============================================
+// Build Tasks
+//===============================================
+
+/** HTML Build - move src html files to dist for dependency injection. */
+gulp.task('build:views', ['clean:views', 'copy:views']);
+
+/**
+ * CSS Build - Call individual CSS tasks from a single build task.
+ * BUG: minify:css has to be run manually after compile:static-content is run.
+ */
 gulp.task('build:css', ['clean:css', 'concat:css', 'minify:css']);
 
-/** CSS Injection - Inject styles into HTML from CSS files to improve performance. */
-gulp.task('inject:css', () => {
-  /** Copy over most recent HTML files. */
-  const stream_a = gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./dist/'));
+/**
+ * JS Build - move src JS files to dist directory.
+ * BUG: minify:js has to be run manually after compile:static-content is run.
+ */
+gulp.task('build:js', ['clean:js', 'copy:js', 'minify:js']);
 
-  /** Inject generated CSS into copied HTML files. */
-  const stream_b = gulp.src('./dist/**/*.html')
-    .pipe(inlineCss())
-    .pipe(gulp.dest('./dist/'));
+/** Image Build - compress image files from source to dist. */
+gulp.task('build:img', ['clean:img', 'compress:img']);
 
-  /** Merge and return stream. */
-  return merge(stream_a, stream_b);
+//===============================================
+// Compile Tasks
+//===============================================
+
+/** Static Content - runs all static content buld tasks before DI. */
+gulp.task('compile:static-content', ['build:views', 'build:css', 'build:js', 'build:img']);
+
+/** Dependency Injection - Inject styles, scripts, and images into HTML from CSS files to improve performance. */
+gulp.task('compile:di', () => {
+  return gulp.src(pkg.globs.dist.views)
+    .pipe(inline({
+      base: pkg.paths.dist,
+      disabledTypes: ['svg', 'img', 'js']
+    }))
+    .pipe(gulp.dest(pkg.paths.dist.views));
 });
